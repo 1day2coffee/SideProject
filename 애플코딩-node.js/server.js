3fb9845b0bb3015928f2c809cc7c3dc558ac2485
JYPARK <jy.park@smartcareworks.com>
2021-11-12 16:17:39 +0900

    create list.ejs page, link and connect mongodb, update add.html page

--- 애플코딩-node.js/server.js
+++ 애플코딩-node.js/server.js
@@ -3,9 +3,22 @@ const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8080, function () {
-  console.log("listening on 8080");
-});
+const MongoClient = require("mongodb").MongoClient;
+app.set("view engine", "ejs");
+
+var db;
+MongoClient.connect(
+  "mongodb+srv://admin:qwer1234@cluster0.sdeif.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  function (에러, client) {
+    if (에러) return console.log(에러);
+
+    db = client.db("todoapp");
+
+    app.listen(8080, function () {
+      console.log("listening on 8080");
+    });
+  }
+);
 
 app.get("/", function (요청, 응답) {
   응답.sendFile(__dirname + "/index.html");
@@ -17,6 +30,40 @@ app.get("/write", function (요청, 응답) {
 
 app.post("/add", function (요청, 응답) {
   응답.send("전송완료");
-  console.log(요청.body);
-  //DB에저장
+
+  //총 게시물갯수 찾기
+  db.collection("counter").findOne(
+    { name: "게시물갯수" },
+    function (에러, 결과) {
+      console.log(결과.totalPost);
+      var 총게시물갯수 = 결과.totalPost;
+
+      //DB에저장
+      db.collection("post").insertOne(
+        { _id: 총게시물갯수 + 1, 제목: 요청.body.title, 날짜: 요청.body.date },
+        function (에러, 결과) {
+          console.log("저장완료");
+          db.collection("counter").updateOne(
+            { name: "게시물갯수" },
+            //$set, $inc 등등은 mongodb operator
+            { $inc: { totalPost: 1 } },
+            function (에러, 결과) {
+              if (에러) return console.log(에러);
+            }
+          );
+        }
+      );
+    }
+  );
+});
+
+// /list로 get요청해서 접속하면 실제 db에 저장된 데이터들로 예쁘게 꾸며진 html을 보여줌
+
+app.get("/list", function (요청, 응답) {
+  db.collection("post")
+    .find()
+    .toArray(function (에러, 결과) {
+      console.log(결과);
+      응답.render("list.ejs", { posts: 결과 });
+    });
 });
