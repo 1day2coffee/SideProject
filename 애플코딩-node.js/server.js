780fb51611c540d50a6501d672bb5ef3956ea2a0
JYPARK <jy.park@smartcareworks.com>
2021-11-16 19:45:19 +0900

    create list, detail, 404 .ejs / create nav.html / create main.css / modify server.js, list.ejs

--- 애플코딩-node.js/server.js
+++ 애플코딩-node.js/server.js
@@ -6,6 +6,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const MongoClient = require("mongodb").MongoClient;
 app.set("view engine", "ejs");
 
+app.use("/public", express.static("public"));
+
 var db;
 MongoClient.connect(
   "mongodb+srv://admin:qwer1234@cluster0.sdeif.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -21,11 +23,11 @@ MongoClient.connect(
 );
 
 app.get("/", function (요청, 응답) {
-  응답.sendFile(__dirname + "/index.html");
+  응답.render("index.ejs");
 });
 
 app.get("/write", function (요청, 응답) {
-  응답.sendFile(__dirname + "/write.html");
+  응답.render("write.ejs");
 });
 
 app.post("/add", function (요청, 응답) {
@@ -57,8 +59,6 @@ app.post("/add", function (요청, 응답) {
   );
 });
 
-// /list로 get요청해서 접속하면 실제 db에 저장된 데이터들로 예쁘게 꾸며진 html을 보여줌
-
 app.get("/list", function (요청, 응답) {
   db.collection("post")
     .find()
@@ -66,4 +66,27 @@ app.get("/list", function (요청, 응답) {
       console.log(결과);
       응답.render("list.ejs", { posts: 결과 });
     });
+});
+
+app.delete("/delete", function (요청, 응답) {
+  console.log(요청.body);
+  요청.body._id = parseInt(요청.body._id);
+  db.collection("post").deleteOne(요청.body, function (에러, 결과) {
+    console.log("삭제완료");
+    응답.status(200).send({ message: "성공했습니다." });
+  });
+});
+
+app.get("/detail/:id", function (요청, 응답) {
+  db.collection("post").findOne(
+    { _id: parseInt(요청.params.id) },
+    function (에러, 결과) {
+      if (응답.status(400)) {
+        응답.render("404.ejs");
+        console.log();
+      } else {
+        응답.render("detail.ejs", { data: 결과 });
+      }
+    }
+  );
 });
